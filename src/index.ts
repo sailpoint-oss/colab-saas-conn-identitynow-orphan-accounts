4e08795d04b004e190f2e7d0d041403641d9acb1
Fernando de los Ríos Sánchez <fernando.delosrios@sailpoint.com>
2024-10-17 18:02:01 +0200

    Rebase

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { AxiosResponse } from 'axios'
 import {
     Context,
     ConnectorError,
@@ -8,7 +7,6 @@ import {
     Response,
     StdAccountCreateInput,
     StdAccountCreateOutput,
-    StdAccountListInput,
     StdAccountListOutput,
     StdAccountReadInput,
     StdAccountReadOutput,
@@ -19,96 +17,120 @@ import {
     StdEntitlementReadInput,
     StdTestConnectionOutput,
     AttributeChangeOp,
+    StdTestConnectionInput,
+    StdAccountListInput,
 } from '@sailpoint/connector-sdk'
-import { IDNClient } from './idn-client'
-import { Account } from './model/account'
+import { OrphanAccount } from './model/account'
 import { Group } from './model/group'
+import { SDKClient } from './sdk-client'
+import { Config } from './model/config'
 
 // Connector must be exported as module property named connector
 export const connector = async () => {
     // Get connector source config
-    const config = await readConfig()
+    const config: Config = await readConfig()
 
     // Use the vendor SDK, or implement own client as necessary, to initialize a client
-    const client = new IDNClient(config)
-
-    const SLEEP = 5 * 1000
-
-    function sleep(ms: number) {
-        return new Promise((resolve) => setTimeout(resolve, ms))
-    }
-
-    const buildSimpleAccount = (id: string, name: string): Account => {
-        const account: Account = {
-            identity: id,
-            uuid: name,
-            attributes: { id, name },
-        }
-
-        return account
-    }
-
-    const makeGroupsRequestable = async (groups: Group[]): Promise<void> => {
-        await sleep(SLEEP)
-        const filters = 'value in (' + groups.map(({ identity }) => `"${identity}"`).join(',') + ')'
-        const response1 = await client.getEntitlements(filters)
-        for (const { id } of response1.data) {
-            const response2 = await client.makeEntitlementRequestable(id)
-        }
-    }
+    const client = new SDKClient(config)
 
     return createConnector()
-        .stdTestConnection(async (context: Context, input: undefined, res: Response<StdTestConnectionOutput>) => {
-            const response: AxiosResponse = await client.testConnection()
-            if (response.status != 200) {
-                throw new ConnectorError('Unable to connect to IdentityNow')
-            } else {
-                res.send({})
+        .stdTestConnection(
+            async (context: Context, input: StdTestConnectionInput, res: Response<StdTestConnectionOutput>) => {
+                try {
+                    const response = await client.publicIdentities()
+                    res.send({})
+                } catch (e) {
+                    throw new ConnectorError('Unable to connect to Identity Security Cloud')
+                }
             }
-        })
+        )
         .stdAccountList(async (context: Context, input: StdAccountListInput, res: Response<StdAccountListOutput>) => {
-            if (config.enableOrphanIdentities) {
-                const response: AxiosResponse = await client.collectOrphanAccounts()
-                for (const acc of response.data) {
-                    const account: Account = new Account(acc)
-
-                    logger.info(account)
+            const sources = await client.listSources()
+            const includedSources = sources.filter((x) => config.sources.includes(x.name))
+            for (const source of includedSources) {
+                const accounts = await client.listOrphanAccountsBySource(source.id!, config.onlyEnabled)
+                for (const acc of accounts) {
+                    const account = new OrphanAccount(acc)
+                    logger.info({ account })
                     res.send(account)
                 }
-            } else {
-                logger.info('Orphan identities disabled, skipping account aggregation...')
             }
-        })
-        .stdAccountRead(async (context: Context, input: StdAccountReadInput, res: Response<StdAccountReadOutput>) => {
-            logger.info(input)
-            const response = await client.getAccount(input.identity)
-            const account: Account = new Account(response.data)
-
-            res.send(account)
         })
         .stdAccountCreate(
             async (context: Context, input: StdAccountCreateInput, res: Response<StdAccountCreateOutput>) => {
-                logger.info(input)
-                const response = await client.getIdentity(input.attributes.name)
-                const id = response.data[0].id
-                await client.correlateAccount(id, input.attributes.id)
-                const account = buildSimpleAccount(input.attributes.id, input.attributes.name)
+                logger.info({ input })
 
-                logger.info(account)
+                const identity = await client.getIdentityByUID(input.attributes.name)
+                await client.correlateAccount(identity!.id, input.identity!)
+                const sourceAccount = await client.getAccount(input.identity!)
+                const account = new OrphanAccount(sourceAccount!)
+
+                logger.info({ account })
                 res.send(account)
+            }
+        )
+        .stdAccountRead(async (context: Context, input: StdAccountReadInput, res: Response<StdAccountReadOutput>) => {
+            logger.info({ input })
+
+            const sourceAccount = await client.getAccount(input.identity!)
+            const account = new OrphanAccount(sourceAccount!)
+
+            logger.info({ account })
+            res.send(account)
+        })
+        .stdAccountDisable(async (context: Context, input: any, res: Response<any>) => {
+            logger.info({ input })
+
+            await client.disableAccount(input.identity)
+            const sourceAccount = await client.getAccount(input.identity!)
+            const account = new OrphanAccount(sourceAccount!)
+
+            logger.info({ account })
+            res.send(account)
+        })
+        .stdAccountEnable(async (context: Context, input: any, res: Response<any>) => {
+            logger.info({ input })
+
+            await client.disableAccount(input.identity)
+            const sourceAccount = await client.getAccount(input.identity!)
+            const account = new OrphanAccount(sourceAccount!)
+
+            logger.info({ account })
+            res.send(account)
+        })
+        .stdEntitlementList(async (context: Context, input: any, res: Response<StdEntitlementListOutput>) => {
+            const sources = await client.listSources()
+            const includedSources = sources.filter((x) => config.sources.includes(x.name))
+            for (const source of includedSources) {
+                const accounts = await client.listOrphanAccountsBySource(source.id!, config.onlyEnabled)
+                for (const acc of accounts) {
+                    const entitlement = new Group(acc)
+                    logger.info({ entitlement })
+                    res.send(entitlement)
+                }
+            }
+        })
+        .stdEntitlementRead(
+            async (context: Context, input: StdEntitlementReadInput, res: Response<StdEntitlementReadOutput>) => {
+                logger.info({ input })
+
+                const sourceAccount = await client.getAccount(input.identity!)
+                const entitlement = new Group(sourceAccount!)
+
+                logger.info({ entitlement })
+                res.send(entitlement)
             }
         )
         .stdAccountUpdate(
             async (context: Context, input: StdAccountUpdateInput, res: Response<StdAccountUpdateOutput>) => {
-                logger.info(input)
-                const response1 = await client.getAccount(input.identity)
-                let account: Account = new Account(response1.data)
-                for (let change of input.changes) {
+                logger.info({ input })
+
+                for (const change of input.changes) {
                     const values = [].concat(change.value)
-                    for (let value of values) {
+                    for (const value of values) {
                         switch (change.op) {
                             case AttributeChangeOp.Add:
-                                console.log('Skipping entitlement add request for orphan account')
+                                logger.info('Skipping entitlement add request for orphan account')
                                 break
                             case AttributeChangeOp.Remove:
                                 await client.disableAccount(value)
@@ -118,54 +140,12 @@ export const connector = async () => {
                         }
                     }
                 }
-                const response2 = await client.getAccount(input.identity)
-                account = new Account(response2.data)
 
-                logger.info(account)
+                const sourceAccount = await client.getAccount(input.identity!)
+                const account = new OrphanAccount(sourceAccount!)
+
+                logger.info({ account })
                 res.send(account)
-            }
-        )
-        .stdAccountDisable(async (context: Context, input: any, res: Response<any>) => {
-            logger.info(input)
-            await client.disableAccount(input.identity)
-            const response = await client.getAccount(input.identity)
-            const account = new Account(response.data)
-
-            logger.info(account)
-            res.send(account)
-        })
-        .stdAccountEnable(async (context: Context, input: any, res: Response<any>) => {
-            logger.info(input)
-            await client.enableAccount(input.identity)
-            const response = await client.getAccount(input.identity)
-            const account = new Account(response.data)
-
-            logger.info(account)
-            res.send(account)
-        })
-        .stdEntitlementList(async (context: Context, input: any, res: Response<StdEntitlementListOutput>) => {
-            const response = await client.collectOrphanAccounts()
-            const accessProfiles: string[] = []
-            const groups: Group[] = []
-            for (const gr of response.data) {
-                const group: Group = new Group(gr)
-                groups.push(group)
-
-                logger.info(group)
-                res.send(group)
-            }
-            if (config.makeEntitlementsRequestable) {
-                makeGroupsRequestable(groups)
-            }
-        })
-        .stdEntitlementRead(
-            async (context: Context, input: StdEntitlementReadInput, res: Response<StdEntitlementReadOutput>) => {
-                logger.info(input)
-                const response: AxiosResponse = await client.getAccount(input.identity)
-                const group: Group = new Group(response.data)
-
-                logger.info(group)
-                res.send(group)
             }
         )
 }
--- src/model/account.ts
+++ src/model/account.ts
@@ -1,23 +1,29 @@
 import { Attributes, StdAccountReadOutput } from '@sailpoint/connector-sdk'
+import { Account } from 'sailpoint-api-client'
 
 const TAG = 'Orphan account'
 
-export class Account {
+export class OrphanAccount implements StdAccountReadOutput {
     identity: string
     uuid: string
     attributes: Attributes
+    disabled: boolean
+    locked: boolean
 
-    constructor(object: any) {
+    constructor(account: Account) {
         this.attributes = {
             tag: TAG,
-            name: object.name === null ? '-' : object.name,
-            displayName: `${TAG}: ${object.name === null ? '-' : object.name}`,
-            id: object.id,
-            description: `Source: ${object.sourceName}`,
-            enabled: !object.disabled,
-            locked: object.locked,
-            source: object.sourceName,
+            name: account.name === null ? '-' : account.name,
+            displayName: `${TAG}: ${account.name === null ? '-' : account.name}`,
+            id: account.id!,
+            description: `Source: ${account.sourceName}`,
+            enabled: !account.disabled,
+            locked: account.locked,
+            source: account.sourceName,
         }
+
+        this.locked = account.locked
+        this.disabled = account.disabled
         this.identity = this.attributes.id as string
         this.uuid = this.attributes.displayName as string
     }
